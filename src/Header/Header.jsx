16f42c6ba2478c5fb77d6d0d7699b13b8048de3e
tanyushanel <tatyana.nelipovich@gmail.com>
2024-03-21 19:22:26 +0100

    feat: header scrolling added

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,10 +1,27 @@
 import style from "./Header.module.scss";
+import React, { useState, useEffect } from "react";
 import { Navigation } from "./../constants/Navigation";
 import cx from "classnames";
 export const Header = () => {
   const nav = Object.values(Navigation);
+  const [isScrolled, setIsScrolled] = useState(false);
+  useEffect(() => {
+    const threshold = document.getElementById("about").offsetTop;
+    const onScroll = () => {
+      // Set state based on scroll position
+      setIsScrolled(window.scrollY > threshold);
+    };
+
+    // Attach scroll event listener to window
+    window.addEventListener("scroll", onScroll);
+
+    // Clean up by removing the event listener
+    // return () => window.removeEventListener("scroll", onScroll);
+    return;
+  }, []);
+
   return (
-    <header className={style.header}>
+    <header className={isScrolled ? style.scrolled : style.header}>
       <div className="logo"></div>
       <nav>
         <ul className={style.menuBox}>
