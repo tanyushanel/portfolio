ca2b93587a34581d204d622b4c0dd937a13d2a95
tanyushanel <tatyana.nelipovich@gmail.com>
2024-03-23 20:01:41 +0100

    feat: fixed pathes

--- src/constants/Socials.js
+++ src/constants/Socials.js
@@ -1,22 +1,22 @@
 export const Socials = {
   linkedin: {
-    img: "/assets/icons/linkedin-icon.svg",
+    img: "./assets/icons/linkedin-icon.svg",
     href: "https://www.linkedin.com/in/tatyana-nelipovich-3a040a1b7/",
   },
   github: {
-    img: "/assets/icons/github-icon.svg",
+    img: "./assets/icons/github-icon.svg",
     href: "https://github.com/tanyushanel",
   },
   codewars: {
-    img: "/assets/icons/codewars-icon.svg",
+    img: "./assets/icons/codewars-icon.svg",
     href: "https://www.codewars.com/users/tanyushanel",
   },
   facebook: {
-    img: "/assets/icons/facebook-icon.svg",
+    img: "./assets/icons/facebook-icon.svg",
     href: "https://www.facebook.com/tanyusha.mikhalchik/",
   },
   instagram: {
-    img: "/assets/icons/instagram-icon.svg",
+    img: "./assets/icons/instagram-icon.svg",
     href: "https://www.instagram.com/tanyusha.nel/",
   },
 };
