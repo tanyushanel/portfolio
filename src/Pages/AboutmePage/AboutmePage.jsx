945d44f75815d8401c3f2113fca7a6f12dfcbfed
tanyushanel <tatyana.nelipovich@gmail.com>
2024-03-23 16:37:54 +0100

    feat: some package.json fixes

--- src/Pages/AboutmePage/AboutmePage.jsx
+++ src/Pages/AboutmePage/AboutmePage.jsx
@@ -2,6 +2,25 @@ export const AboutmePage = () => {
   return (
     <article className="article-about" id="about">
       <h2 className="art-heading">About Me</h2>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium
+        delectus at eum, earum praesentium accusantium dolorum blanditiis,
+        natus, minus maiores dignissimos repellat officiis corporis culpa quas
+        sint consequuntur ratione unde. Ex, quas tempore! Dolores quisquam
+        adipisci ut placeat facere iste commodi voluptates? Culpa, ducimus.
+        Obcaecati rem sed pariatur ratione ipsum, repellendus ab aliquid libero
+        possimus, laudantium reprehenderit fugit cupiditate minima.
+        Reprehenderit unde laboriosam soluta, fugiat, provident beatae eligendi
+        eum, neque facere ab deserunt aperiam voluptas aspernatur odit. Dolores
+        sint veritatis quo aperiam, quae ad commodi harum, pariatur omnis quasi
+        sunt. Minus aliquam soluta amet quae delectus aperiam perferendis saepe.
+        Neque, dolore impedit temporibus ipsa, quia eveniet deleniti veniam
+        fugiat architecto, commodi similique enim vel suscipit. Delectus illum
+        ex iste unde. Omnis error labore animi consectetur totam fuga unde
+        consequatur! Omnis consequatur a possimus ullam tempore repellendus
+        animi minima expedita, quae sint reprehenderit ratione, eos iure sunt
+        architecto autem quaerat nobis.
+      </p>
     </article>
   );
 };
