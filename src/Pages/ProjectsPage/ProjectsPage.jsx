7f9b4b2bc30beca399c834e00047a1d34fb37f16
tanyushanel <tatyana.nelipovich@gmail.com>
2024-05-14 23:08:15 +0200

    feat: added project

--- src/Pages/ProjectsPage/ProjectsPage.jsx
+++ src/Pages/ProjectsPage/ProjectsPage.jsx
@@ -1,7 +1,17 @@
+import resource from "../../assets/images/projects/resource.png";
+import style from "./ProjectPage.module.scss";
+
 export const ProjectsPage = () => {
   return (
     <article className="article-projects" id="projects">
       <h2>My Projects</h2>
+      <ul>
+        <li className={style.gridItem}>
+          <a href="https://tanyushanel.github.io/coffee-shop-react">
+            <img src={resource} alt="source" />
+          </a>
+        </li>
+      </ul>
     </article>
   );
 };
